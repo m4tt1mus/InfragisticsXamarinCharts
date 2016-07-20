524f9efa266118af74e67cf9c88e3e1062c583ab
Matt Seigel <matt.seigel@headspring.com>
2016-07-20 16:05:07 -0500

    add reactiveui and created datapointpage

---
+++ InfragisticXamarinCharts/InfragisticXamarinCharts/Models/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿namespace InfragisticXamarinCharts.Models
+{
+    using System;
+    using ReactiveUI;
+
+    public class DataPoint : ReactiveObject
+    {
+        private double _value;
+        public double Value
+        {
+            get { return _value; }
+            set { this.RaiseAndSetIfChanged(ref _value, value); }
+        }
+
+        private DateTime _timestamp;
+        public DateTime Timestamp
+        {
+            get { return _timestamp; }
+            set { this.RaiseAndSetIfChanged(ref _timestamp, value); }
+        }
+    }
+}
