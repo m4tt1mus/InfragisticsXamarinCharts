52231c452afc2df225b77a33a492b2a1fb9d0b9b
Matt Seigel <matt.seigel@headspring.com>
2016-07-21 09:41:28 -0500

    created a view model for the new page

---
+++ InfragisticXamarinCharts/InfragisticXamarinCharts/Models/DataPointPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfragisticXamarinCharts.Models
+{
+    using ReactiveUI;
+
+    public class DataPointPageViewModel : ReactiveObject
+    {
+    }
+}
